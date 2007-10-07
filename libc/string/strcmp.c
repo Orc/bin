571a8ca3e192e8e09290a797ea6bbed1e29f449a
david parsons <orc@pell.portland.or.us>
2007-10-07 07:30:44 -0700

    Add strcmp()

---
+++ libc/string/strcmp.c
@@ -0,0 +1,46 @@
+#include <string.h>
+
+int
+strcmp(const char*s1, const char*s2)
+{
+    int cmp;
+
+    asm( "cld\n"
+       "1:lodsb\n"
+       "  scasb\n"
+       "  jne 2f\n"
+       "  orb %%al,%%al\n"
+       "  jne 1b\n"
+       "  jmp 3f\n"
+       "2:sbbl %%eax,%%eax\n"
+       "  orb $1, %%al\n"
+       "3:"
+       : "=a" (cmp)
+       : "S" (s1), "D" (s2), "a" (0)
+       : "%esi", "%edi" );
+    return cmp;
+}
+
+
+#if TEST
+
+void
+test(char *a, char *b)
+{
+    printf("strcmp(\"%s\",\"%s\") = %d\n", a, b, strcmp(a,b));
+}
+
+
+main()
+{
+    test("a", "a");
+    test("a", "b");
+    test("b", "a");
+    test("aaa", "aaa");
+    test("aaa", "aab");
+    test("aaa", "aba");
+    test("aab", "aaa");
+    test("aba", "aaa");
+}
+
+#endif
