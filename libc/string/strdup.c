c86b3d5edccb350ef72336da97b5eccc59b69bed
david parsons <orc@pell.portland.or.us>
2007-10-07 08:11:40 -0700

    Add strncat()

---
+++ libc/string/strdup.c
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdlib.h>
+
+char*
+strdup(const char* src)
+{
+    char *res;
+    int   siz;
+
+    if ( !src ) return 0;
+
+    /* get strlen(src)+1 (for trailing null) */
+    asm("cld\n"
+       " repne\n"
+       " scasb\n"
+       " notl %0\n"
+       : "=c" (siz)
+       : "D" (src), "a" (0), "c" (-1L)
+       : "%edi");
+
+#if TEST
+    printf("in strdup: siz+1 = %d\n", siz);
+#endif
+
+    if ( res = malloc(siz) ) 
+	/* allocate and copy all src + trailing null */
+	asm("cld\n"
+	   " rep\n"
+	   " movsb"
+	    : /*this space intentionally left blank*/
+	    : "S"(src), "D"(res), "c"(siz)
+	    : "%edi", "%esi");
+    return res;
+}
+
+
+#if TEST
+
+void
+test(char *src)
+{
+    char *res = strdup(src);
+
+    printf("strdup(\"%s\") = \"%s\"\n", src ? src : "null", res ? res : "null");
+}
+
+main()
+{
+    test("hello, sailor");
+    test(NULL);
+    test("hello");
+    test("boys");
+}
+#endif
---
+++ libc/string/strncat.c
@@ -0,0 +1,68 @@
+#include <string.h>
+
+char *
+strncat(char* dest, const char* src, size_t siz)
+{
+    /* skid to the end of the string, then strncpy the new string in */
+    if (siz) 
+	asm( "cld\n"
+	   "  repne\n"
+	   "  scasb\n"
+	   "  dec %%edi\n"
+	   "1:lodsb\n"
+	   "  stosb\n"
+	   "  dec %%ebx\n"
+	   "  jz 2f\n"
+	   "  orb %%al,%%al\n"
+	   "  jnz 1b\n"
+	   "2:"
+	   : /* this space intentionally left blank */
+	   : "D" (dest), "S" (src), "a" (0), "b" (siz), "c" (-1L)
+	   : "%edi", "%esi", "%ecx" );
+    return dest;
+}
+
+
+#if TEST
+
+#include <stdio.h>
+#include <stdlib.h>
+
+test(char *dest, char *src, size_t siz)
+{
+    char *res;
+    int count;
+    char *buffer = alloca(strlen(dest) + strlen(src) + 10 + siz);
+
+    memset(buffer, '?', strlen(dest) + strlen(src) + 10 + siz);
+    strcpy(buffer, dest);
+    printf("strncat(\"%s\", \"%s\", %d) = ",
+		    dest ? dest : "null",
+		    src ? src : "null", siz);
+
+    if ( res = strncat(buffer, src, siz) ) {
+	if (res != buffer)
+	    printf("-oops-");
+	putchar('"');
+	for (count=strlen(dest)+siz+1; *res && count; --count,++res)
+	    if (*res >= ' ' && *res <= '~')
+		putchar(*res);
+	    else if (*res == 0)
+		printf("\\0");
+	    else
+		printf("<%02x>", *res);
+	putchar('"');
+	putchar('\n');
+    }
+    else
+	puts("null?");
+}
+
+
+main()
+{
+    test("hello,", " sailor", 80);
+    test("hello,", " sailor", 4);
+}
+
+#endif
