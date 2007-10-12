cc015cb46b9dc0db3876ccbf0c654b7dec3b462d
David Parsons <orc@poison.pell.portland.or.us>
2007-10-11 21:02:16 -0700

    Add a copyright notice, strip out the pushdir/popdir logic (not needed, because (a) I don't do ls -R and (b) that directory handling can be done by chdir(e) ... chdir(..)

--- ls.c
+++ ls.c
@@ -1,3 +1,38 @@
+/*
+ * ls - list directory contents.
+ * 
+ *		Copyright (C) 2007 David Loren Parsons.
+ *			All rights reserved.
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation files
+ *  (the "Software"), to deal in the Software without restriction,
+ *  including without limitation the rights to use, copy, modify, merge,
+ *  publish, distribute, sublicence, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so,
+ *  subject to the following conditions:
+ *
+ *   1) Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions, and the following disclaimer.
+ *
+ *   2) Except as contained in this notice, the name of David Loren
+ *      Parsons shall not be used in advertising or otherwise to promote
+ *      the sale, use or other dealings in this Software without prior
+ *      written authorization from David Loren Parsons.
+ *
+ *  THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ *  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL DAVID LOREN PARSONS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ *  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <stdio.h>
 #include <grp.h>
 #include <pwd.h>
@@ -59,42 +94,7 @@ int psuvm = 0;		/* use internet time */
 int dirblocks = 0;	/* print ``total %d'' line before directory listins  */
 int needstat = 0;	/* depends on options */
 
-
-unsigned *pp_stack = 0;
-unsigned pp_top=0, pp_size = 0;
-
-int
-pushdir(char *dir)
-{
-    if (pp_top >= pp_size) {
-	if (pp_size == 0) {
-	    pp_size = pp_top + 200;
-	    pp_stack = malloc(pp_size * sizeof pp_stack[0]);
-	}
-	else {
-	    pp_size = pp_top * 10;
-	    pp_stack = realloc(pp_stack, pp_size * sizeof pp_stack[0]);
-	}
-    }
-    pp_stack[pp_top] = open(".", O_RDONLY);
-
-    if ( chdir(dir) == -1 ) {
-	close(pp_stack[pp_top]);
-	return -1;
-    }
-    else pp_top++;
-}
-
-
-void
-popdir()
-{
-    if (pp_top) {
-	fchdir(pp_stack[--pp_top]);
-	close(pp_stack[pp_top]);
-    }
-}
-
+int home;		/* fd of starting directory (for fchdir) */
 
 typedef struct {
     int key;
@@ -285,8 +285,10 @@ package(char *path, pack *p)
     DIR *d;
     struct dirent *de;
 
-    if (pushdir(path) == -1)
+    if (chdir(path) == -1) {
+	perror(path);
 	return -1;
+    }
 
     if (d = opendir(".")) {
 	newer(p);
@@ -298,7 +300,7 @@ package(char *path, pack *p)
 	}
 	closedir(d);
 	mkindex(p);
-	popdir();
+	fchdir(home);
 	return p->nrf;
     }
     else return -1;
@@ -652,6 +654,11 @@ main(int argc, char **argv)
 
     columns = isatty(0);
     pgm = basename(argv[0]);
+    
+    if ( (home = open(".", O_RDONLY)) == -1 ) {
+	perror("can't get working directory");
+	exit(1);
+    }
 
     columns = isatty(0);
 
@@ -738,7 +745,7 @@ main(int argc, char **argv)
 	/* list files first */
 	for (i=0; i < argc; i++)
 	    if ( ((follow ? stat : lstat)(argv[i],&st)) == -1 ) {
-		fprintf(stderr, "%s: ", pgm); perror(argv[i]);
+		perror(argv[i]);
 		continue;
 	    }
 	    else  if (directories && S_ISDIR(st.st_mode))
