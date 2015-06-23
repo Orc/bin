20bd9bba69fe5cd64b1e2cbad344771164aa993a
david parsons <orc@pie.local>
2015-06-23 10:07:04 -0700

    Update my email address

--- id.c
+++ id.c
@@ -14,7 +14,7 @@
  *     software must display the following acknowledgement:
  *     
  *   This product includes software developed by David Parsons
- *   (orc@pell.chi.il.us)
+ *   (orc@pell.portland.or.us)
  *
  *  4. My name may not be used to endorse or promote products derived
  *     from this software without specific prior written permission.
